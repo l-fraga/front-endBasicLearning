efd46a1e1d743917041e021266025c902705cbed
Lucas de Fraga Silva <110699870+l-fraga@users.noreply.github.com>
2023-05-03 16:23:13 -0300

    add front-end files
    HTML, CCS and JS basic.

---
+++ Front-end/HTML_CSS_JS/exerciciosBasicos.js
@@ -0,0 +1,38 @@
+let wheight = 75
+
+//console.log(typeof wheight)
+
+/*let name = 'Lucas'
+let age = 19
+let starts = 10.5
+let isSubscribed = true*/
+
+let student = {
+    name : 'Lucas',
+    age : 19,
+    weight : 70.5,
+    isSubscribed : true
+
+}
+
+/*console.log(`${student.name} de idade ${student.age} pesa 
+${student.weight}kg.`)*/
+
+let students = []
+
+students = [
+    student
+]
+
+//console.log(students[0])
+
+const rafa = {
+    name : 'Rafael',
+    age : 19,
+    weight : 80,
+    isSubscribed : true
+}
+
+students[1] = rafa
+
+console.log(students)
---
+++ Front-end/HTML_CSS_JS/scripts.js
@@ -0,0 +1,18 @@
+// getElementById()
+const element = document.getElementById('blog-title')
+console.log(element)
+
+/* getElementByClassName()
+const element = document.getElementsByClassName('one')
+console.log(element)*/
+
+/* getElementByTagName()
+const element = document.getElementsByTagName('h1')
+console.log(element)*/
+
+
+/*const element = document.querySelector('one')
+console.log(element)*/
+
+/*const element = document.querySelectorAll('#one') // # ou . igual css
+console.log(element)*/
